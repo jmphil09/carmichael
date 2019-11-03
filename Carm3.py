bd17eeeac8bf20a08d80de3690007b54726d6096
James Phillips <jmphil09@gmail.com>
2019-11-03 01:57:04 -0800

    Refactored the code and created Carm3 class.

---
+++ Carm3.py
@@ -0,0 +1,108 @@
+import glob
+import pickle
+import random
+
+import numpy as np
+
+from multiprocessing import Pool
+from pathlib import Path
+
+from util import is_carm, primesfrom2to
+
+
+class Carm3:
+    def __init__(self, upper_bound=1000, num_cores=1, data_folder='data'):
+        self.upper_bound = upper_bound
+        self.num_cores = num_cores
+        self.data_folder = data_folder
+        self.primes_to_check = primesfrom2to(self.upper_bound)
+        self.p1_primes_to_check = self.primes_to_check
+
+    def calculate(self):
+        # Load primes_to_check from pickle file
+        file_list = glob.glob(str(Path(self.data_folder + '/' + 'checked_carm_*')))
+        if file_list:
+            max_file_num = max([int(item.replace(str(Path(self.data_folder + '/' + 'checked_carm_')), '').replace('.pkl', '')) for item in file_list])
+            p1_set = set()
+            p1_list = []
+            for n in range(1, max_file_num + 1):
+                try:
+                    filename = Path(self.data_folder + '/' + 'checked_carm_{}.pkl'.format(n))
+                    infile = open(filename, 'rb')
+                    existing_results = pickle.load(infile)
+                    p1_list = p1_list + existing_results
+                    infile.close()
+                except Exception as e:
+                    print(e)
+            p1_set = set(p1_list)
+            self.p1_primes_to_check = [p for p in self.primes_to_check if p not in p1_set]
+        else:
+            pass
+
+        # Split primes into num_cores even segments
+        random.shuffle(self.primes_to_check)
+        n = self.num_cores - 1
+        n_list_size = int(len(self.primes_to_check) / self.num_cores)
+        '''split_primes = (
+            [list(self.primes_to_check[i * n_list_size:(i + 1) * n_list_size]) for i in range(0, n)] + [list(self.primes_to_check[(n) * n_list_size::])]
+        )'''
+        split_primes = np.array_split(self.primes_to_check, self.num_cores)
+
+        p = Pool(processes=self.num_cores)
+        p.starmap(self._generate_3carm, [[i, split_primes[i]] for i in range(0, self.num_cores)])
+
+    def _generate_3carm(self, core_num, p1_primes):
+        p1_primes_to_check = [p for p in p1_primes if p in self.p1_primes_to_check]
+        for p1 in p1_primes_to_check:
+            p2_primes_to_check = [p for p in self.primes_to_check if p > p1]
+            for p2 in p2_primes_to_check:
+                p3_primes_to_check = [p for p in p2_primes_to_check if p > p2]
+                for p3 in p3_primes_to_check:
+                    candidate = p1 * p2 * p3
+                    if is_carm(int(candidate)):
+                        existing_results = []
+                        new_result = [(p1, p2, p3, candidate)]
+                        filename = Path(self.data_folder + '/' + 'found_carm_{}.pkl'.format(core_num))
+                        try:
+                            save_dir = filename.parent
+                            save_dir.mkdir(parents=True, exist_ok=True)
+                            infile = open(filename, 'rb')
+                            existing_results = pickle.load(infile)
+                            infile.close()
+                            result = existing_results + new_result
+                        except Exception as e:
+                            print(e)
+                            result = new_result
+                        outfile = open(filename, 'wb')
+                        pickle.dump(result, outfile)
+                        outfile.close()
+            filename = Path(self.data_folder + '/' + 'checked_carm_{}.pkl'.format(core_num))
+            try:
+                infile = open(filename, 'rb')
+                existing_results = pickle.load(infile)
+                infile.close()
+                result = existing_results + [p1]
+            except Exception as e:
+                print(e)
+                result = [p1]
+            outfile = open(filename, 'wb')
+            pickle.dump(result, outfile)
+            outfile.close()
+
+    def _display_results(self):
+        carm_list = []
+        file_list = glob.glob(str(Path(self.data_folder + '/' + 'found_carm_*')))
+        if file_list:
+            max_file_num = max([int(item.replace(str(Path(self.data_folder + '/' + 'found_carm_')), '').replace('.pkl', '')) for item in file_list])
+            for n in range(1, max_file_num + 1):
+                try:
+                    filename = Path(self.data_folder + '/' + 'found_carm_{}.pkl'.format(n))
+                    infile = open(filename, 'rb')
+                    existing_results = pickle.load(infile)
+                    carm_list = carm_list + existing_results
+                    infile.close()
+                except Exception as e:
+                    print(e)
+        carm_list = sorted(list(set(carm_list)))
+        print(sorted(carm_list))
+        print(len(carm_list))
---
+++ util.py
@@ -0,0 +1,25 @@
+import numpy
+
+from math import gcd
+
+
+# TODO: Replace this with a faster version.
+def is_carm(n):
+    """Check if n is a Carmichael number."""
+    for b in range(2, n):
+        if gcd(b, n) == 1:
+            if pow(b, n-1, n) != 1:
+                return False
+    return True
+
+
+# Consider replacing this with a better version at some point.
+def primesfrom2to(n):
+    """Input n>=6, Returns list of primes, 2 <= p < n ."""
+    sieve = numpy.ones(n//3 + (n % 6 == 2), dtype=numpy.bool)
+    for i in range(1, int(n**0.5)//3+1):
+        if sieve[i]:
+            k = 3*i+1 | 1
+            sieve[k*k//3::2*k] = False
+            sieve[k*(k-2*(i & 1)+4)//3::2*k] = False
+    return numpy.r_[2, 3, ((3*numpy.nonzero(sieve)[0][1:]+1) | 1)]
