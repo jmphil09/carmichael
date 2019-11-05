cd06f2fe9400ad2883b8065149e2a0c3d06b7cc6
James Phillips <jmphil09@gmail.com>
2019-11-04 18:58:17 -0800

    Added 10^6 example.

---
+++ example_1000000.py
@@ -0,0 +1,12 @@
+from Carm3 import Carm3
+
+import time
+start_time = time.perf_counter()
+
+
+if __name__ == '__main__':
+    c3 = Carm3(upper_bound=1000000, num_cores=16, data_folder='carm_1000000')
+    c3.calculate()
+    end_time = time.perf_counter()
+    print("Completed in {} seconds.".format(end_time - start_time))
+    c3._display_results()
