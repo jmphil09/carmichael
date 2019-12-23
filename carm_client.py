9e1dfbd6c8957d1ebd48b2a251c0a1ca7aeb0570
James Phillips <jmphil09@gmail.com>
2019-12-23 00:56:07 -0800

    More client/server updates, and preparation for database.

--- carm_client.py
+++ carm_client.py
@@ -1,33 +1,27 @@
+import json
 import requests
 from shutil import rmtree
 
 from CarmCalculator import CarmCalculator
 
 
-URL = 'http://192.168.50.160:9000/'
+# TODO: Put these values in a config file
+WORKLOAD_URL = 'http://192.168.50.160:9000/get_workload'
+RESULT_URL = 'http://192.168.50.160:9000/send_results'
 NUM_CORES = 16
+BATCH_SIZE = 99
 PARAMS = {
-    'num_cores': NUM_CORES
+    'num_cores': NUM_CORES,
+    'batch_size': BATCH_SIZE
 }
 
 
-response = requests.get(url=URL, params=PARAMS)
-response = response.json()
-algorithm_to_use = response['algorithm_to_use']
-numbers_to_compute = response['numbers_to_compute']
-range_start = numbers_to_compute[0]
-range_stop = numbers_to_compute[1]
-range_step = numbers_to_compute[2]
-
-print(response)
-
-
-def main():
-    if algorithm_to_use=='carm3':
+def main(algorithm_to_use, range_start, range_stop, num_cores):
+    if algorithm_to_use=='carm3':  # TODO: Add support for other algorithms
         c3 = CarmCalculator(
             lower_bound=range_start,
             upper_bound=range_stop,
-            num_cores=NUM_CORES
+            num_cores=num_cores
         )
         result = c3.calc_3_carms()
         return result
@@ -36,12 +30,27 @@ def main():
 
 
 if __name__ == '__main__':
-    calculate_more = True
-    while calculate_more:
-        result = main()
+    finished = False
+    while not finished:
+        import time
+        start_time = time.perf_counter()
+        workload_response = requests.get(url=WORKLOAD_URL, params=PARAMS).json()
+        print(workload_response)
+        algorithm_to_use = workload_response['algorithm_to_use']
+        numbers_to_compute = workload_response['numbers_to_compute']
+        finished = workload_response['finished']
+        range_start = numbers_to_compute[0]
+        range_stop = numbers_to_compute[1]
+
+        result = main(algorithm_to_use, range_start, range_stop, NUM_CORES)
 
         #POST the result to the server
-        print(result)
-
-        #The server will return True or False
-        calculate_more = False
+        json_result = {
+            'result': result
+        }
+        requests.post(RESULT_URL, json=json_result)
+        end_time = time.perf_counter()
+        run_time = int(end_time - start_time)
+        # TODO: If run_time > 10 mins (or some other value), decrease batch_size
+        # If run_time < 1 min, increase the batch_size
+        print("Completed in {} seconds.".format(run_time))
--- start_server.py
+++ start_server.py
@@ -1,18 +1,45 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
-def get_json_response():
-    return {
-        "algorithm_to_use": "carm3",
-        "numbers_to_compute": (300, 800, 1)
+def get_json_response(batch_size):
+    #TODO: Get 'batch_size' items from the worker queue DB
+    # Add these values in memory to current_workload_list
+    # if the values are in current_workload_list for > 60 mins, remove them
+    #Idea: get batch_size items from DB that are NOT in current_workload_list
+    algorithm_to_use = 'carm3'  # Also determined from worker queue DB
+    numbers_to_compute = (3, 10000)  # Create a range based on batch size
+    finished = True  # True if DB is empty, False if otherwise
+
+    json_response = {
+        'algorithm_to_use': algorithm_to_use,
+        'numbers_to_compute': numbers_to_compute,
+        'finished': finished
     }
 
-@app.route('/')
-def example():
-    json_response = get_json_response()
     return json_response
 
+@app.route('/get_workload', methods=['GET'])
+def get_workload():
+    batch_size = request.args.get('batch_size')
+    if batch_size==None:
+        batch_size = 100
+    json_response = get_json_response(batch_size)
+    print(batch_size)
+    return json_response
+
+@app.route('/send_results', methods=['POST'])
+def send_results():
+    # TODO: store the results in the results DB
+
+    # TODO: remove the computed numbers from the worker queue DB
+    # remove the computed numbers from current_workload_list
+
+    print('====Got Results====')
+    print(request.json)
+    print('==== ====')
+    return 'False'
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9000)
+    app.run(host='0.0.0.0', port=9000, debug=True)
