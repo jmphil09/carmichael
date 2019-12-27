7482dfbe51ebb1b78108e67cbe99731ca121e8f8
James Phillips <jmphil09@gmail.com>
2019-12-26 23:40:52 -0800

    Got the client and server pieces working with local variables. Now ready to start using DB's!

--- carm_client.py
+++ carm_client.py
@@ -9,18 +9,16 @@ from CarmCalculator import CarmCalculator
 WORKLOAD_URL = 'http://192.168.50.160:9000/get_workload'
 RESULT_URL = 'http://192.168.50.160:9000/send_results'
 NUM_CORES = 16
-BATCH_SIZE = 99
-PARAMS = {
-    'num_cores': NUM_CORES,
-    'batch_size': BATCH_SIZE
-}
+BATCH_SIZE = 100
 
 
-def main(algorithm_to_use, range_start, range_stop, num_cores):
+
+def main(algorithm_to_use, range_start, range_stop, batch, num_cores):
     if algorithm_to_use=='carm3':  # TODO: Add support for other algorithms
         c3 = CarmCalculator(
             lower_bound=range_start,
             upper_bound=range_stop,
+            batch=batch,
             num_cores=num_cores
         )
         result = c3.calc_3_carms()
@@ -32,6 +30,10 @@ def main(algorithm_to_use, range_start, range_stop, num_cores):
 if __name__ == '__main__':
     finished = False
     while not finished:
+        PARAMS = {
+            'num_cores': NUM_CORES,
+            'batch_size': BATCH_SIZE
+        }
         import time
         start_time = time.perf_counter()
         workload_response = requests.get(url=WORKLOAD_URL, params=PARAMS).json()
@@ -39,18 +41,25 @@ if __name__ == '__main__':
         algorithm_to_use = workload_response['algorithm_to_use']
         numbers_to_compute = workload_response['numbers_to_compute']
         finished = workload_response['finished']
-        range_start = numbers_to_compute[0]
-        range_stop = numbers_to_compute[1]
+        range_start = min(numbers_to_compute)
+        range_stop = max(numbers_to_compute)
 
-        result = main(algorithm_to_use, range_start, range_stop, NUM_CORES)
+        result = main(algorithm_to_use, range_start, range_stop, numbers_to_compute, NUM_CORES)
 
         #POST the result to the server
         json_result = {
             'result': result
         }
         requests.post(RESULT_URL, json=json_result)
+
         end_time = time.perf_counter()
         run_time = int(end_time - start_time)
-        # TODO: If run_time > 10 mins (or some other value), decrease batch_size
-        # If run_time < 1 min, increase the batch_size
         print("Completed in {} seconds.".format(run_time))
+        if run_time < 60: # If run_time < 1 min: increase the batch_size
+            BATCH_SIZE = 2 * BATCH_SIZE
+            print('Increasing batch size to {}'.format(BATCH_SIZE))
+        elif run_time > 600: # If run_time > 10 mins: decrease batch_size
+            BATCH_SIZE = int(BATCH_SIZE / 2)
+            print('Decreasing batch size to {}'.format(BATCH_SIZE))
+        else:
+            print('Keeping batch size at {}'.format(BATCH_SIZE))
--- start_server.py
+++ start_server.py
@@ -2,15 +2,19 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+GLOBAL_NUMS_TO_COMPUTE = list(range(3, 1000)) #TODO: replace with DB
+GLOBAL_RESULTS = [] #TODO: replace with DB
 
 def get_json_response(batch_size):
+    global GLOBAL_NUMS_TO_COMPUTE
     #TODO: Get 'batch_size' items from the worker queue DB
     # Add these values in memory to current_workload_list
     # if the values are in current_workload_list for > 60 mins, remove them
     #Idea: get batch_size items from DB that are NOT in current_workload_list
     algorithm_to_use = 'carm3'  # Also determined from worker queue DB
-    numbers_to_compute = (3, 10000)  # Create a range based on batch size
-    finished = True  # True if DB is empty, False if otherwise
+    numbers_to_compute = GLOBAL_NUMS_TO_COMPUTE[:batch_size]  # Create a list based on batch size
+    GLOBAL_NUMS_TO_COMPUTE = GLOBAL_NUMS_TO_COMPUTE[batch_size:]
+    finished = (GLOBAL_NUMS_TO_COMPUTE == [])  # True if DB is empty, False if otherwise
 
     json_response = {
         'algorithm_to_use': algorithm_to_use,
@@ -22,7 +26,7 @@ def get_json_response(batch_size):
 
 @app.route('/get_workload', methods=['GET'])
 def get_workload():
-    batch_size = request.args.get('batch_size')
+    batch_size = int(request.args.get('batch_size'))
     if batch_size==None:
         batch_size = 100
     json_response = get_json_response(batch_size)
@@ -31,13 +35,16 @@ def get_workload():
 
 @app.route('/send_results', methods=['POST'])
 def send_results():
+    global GLOBAL_RESULTS
     # TODO: store the results in the results DB
 
     # TODO: remove the computed numbers from the worker queue DB
     # remove the computed numbers from current_workload_list
 
     print('====Got Results====')
-    print(request.json)
+    #print(request.json['result'])
+    GLOBAL_RESULTS = GLOBAL_RESULTS + request.json['result']
+    print(GLOBAL_RESULTS)
     print('==== ====')
     return 'False'
 
