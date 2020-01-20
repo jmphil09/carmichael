76434526991001457edfe057339522a45b1759a4
James Phillips <jmphil09@gmail.com>
2020-01-20 02:36:05 -0800

    Got pi4 working!!!!

--- pi4_client.py
+++ pi4_client.py
@@ -37,43 +37,45 @@ if __name__ == '__main__':
     finished = False
     git_counter = 0
     while True:
-        git_counter += 1
-        if git_counter % GIT_MOD_COUNTER == 0:
-            os.system(GIT_COMMAND)
-            git_counter = 0
-        if finished:
+        try:
+            git_counter += 1
+            if git_counter % GIT_MOD_COUNTER == 0:
+                os.system(GIT_COMMAND)
+                git_counter = 0
+
+            PARAMS = {
+                'num_cores': NUM_CORES,
+                'batch_size': BATCH_SIZE
+            }
+            start_time = time.perf_counter()
+            workload_response = requests.get(url=WORKLOAD_URL, params=PARAMS).json()
+            print(workload_response)
+            algorithm_to_use = workload_response['algorithm_to_use']
+            numbers_to_compute = workload_response['numbers_to_compute']
+            finished = workload_response['finished']
+
+            range_start = min(numbers_to_compute)
+            range_stop = max(numbers_to_compute)
+
+            result = main(algorithm_to_use, range_start, range_stop, numbers_to_compute, NUM_CORES)
+
+            #POST the result to the server
+            json_result = {
+                'result': result
+            }
+            requests.post(RESULT_URL, json=json_result)
+
+            end_time = time.perf_counter()
+            run_time = int(end_time - start_time)
+            print("Completed in {} seconds.".format(run_time))
+            if run_time < BATCH_INCREASE_TIME: # If run_time < 1 min: increase the batch_size
+                BATCH_SIZE = 2 * BATCH_SIZE
+                print('Increasing batch size to {}'.format(BATCH_SIZE))
+            elif run_time > 600: # If run_time > 10 mins: decrease batch_size
+                BATCH_SIZE = int(BATCH_SIZE / 2)
+                print('Decreasing batch size to {}'.format(BATCH_SIZE))
+            else:
+                print('Keeping batch size at {}'.format(BATCH_SIZE))
+        except Exception as ex:
+            print(ex)
             time.sleep(WAIT_TIME)
-
-        PARAMS = {
-            'num_cores': NUM_CORES,
-            'batch_size': BATCH_SIZE
-        }
-        start_time = time.perf_counter()
-        workload_response = requests.get(url=WORKLOAD_URL, params=PARAMS).json()
-        print(workload_response)
-        algorithm_to_use = workload_response['algorithm_to_use']
-        numbers_to_compute = workload_response['numbers_to_compute']
-        finished = workload_response['finished']
-
-        range_start = min(numbers_to_compute)
-        range_stop = max(numbers_to_compute)
-
-        result = main(algorithm_to_use, range_start, range_stop, numbers_to_compute, NUM_CORES)
-
-        #POST the result to the server
-        json_result = {
-            'result': result
-        }
-        requests.post(RESULT_URL, json=json_result)
-
-        end_time = time.perf_counter()
-        run_time = int(end_time - start_time)
-        print("Completed in {} seconds.".format(run_time))
-        if run_time < BATCH_INCREASE_TIME: # If run_time < 1 min: increase the batch_size
-            BATCH_SIZE = 2 * BATCH_SIZE
-            print('Increasing batch size to {}'.format(BATCH_SIZE))
-        elif run_time > 600: # If run_time > 10 mins: decrease batch_size
-            BATCH_SIZE = int(BATCH_SIZE / 2)
-            print('Decreasing batch size to {}'.format(BATCH_SIZE))
-        else:
-            print('Keeping batch size at {}'.format(BATCH_SIZE))
