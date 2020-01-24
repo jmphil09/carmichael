b9c1197bfc0d911d58d05decbc9745cf844d8ca7
James Phillips <jmphil09@gmail.com>
2020-01-23 17:25:04 -0800

    Fixed a bug in status_check.py.

--- status_check.py
+++ status_check.py
@@ -12,4 +12,4 @@ num_completed = len(completed)
 remaining = retrieve_items(100000, table='numbers_to_compute', user=USER, password=PASSWORD, host=HOST, port=PORT, database='numbers_to_compute')
 num_remaining = len(remaining)
 
-print("Number completed: {}, number remaining: {}, percentage complete: {}".format(num_completed, num_remaining, 100*(num_completed / num_remaining)))
+print("Number completed: {}, number remaining: {}, percentage complete: {}".format(num_completed, num_remaining, 100*(num_completed / (num_completed + num_remaining))))
