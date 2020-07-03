7f15a53740921a9193e037eaad36c40edf23d6cc
James Phillips <jmphil09@gmail.com>
2020-07-03 15:46:23 -0700

    Updated results.py to include exceptional primes.

--- results.py
+++ results.py
@@ -1,6 +1,8 @@
 import db_commands
 import db_config
 
+from util import primesfrom2to
+
 
 USER = db_config.USER
 PASSWORD = db_config.PASSWORD
@@ -10,7 +12,22 @@ DATABASE = db_config.DATABASE
 DATABASE_RESULTS = db_config.DATABASE_RESULTS
 
 
-#db_commands.get_results()
 results = db_commands.retrieve_items(1000000, table='results', user=USER, password=PASSWORD, host=HOST, port=PORT, database='numbers_to_compute')
 
+p1s = sorted(list(set([factors[0] for factors in results])))
+p2s = sorted(list(set([factors[1] for factors in results])))
+p3s = sorted(list(set([factors[2] for factors in results])))
+possible_p1s = primesfrom2to(p1s[-1] + 1)
+exceptional_primes = [p for p in possible_p1s if p not in p1s]
+exceptional_primes = [p for p in exceptional_primes if p > 2]
+
+
+print('=======================================================')
 print('Number of 3-Carmichael numbers with p1 < 1,000,000: {}'.format(len(results)))
+print('Number of exception primes with p1 < 1,000,000: {}'.format(len(exceptional_primes)))
+print('=======================================================')
+
+print('=======================================================')
+print('Exceptional Primes')
+print(exceptional_primes)
+print('=======================================================')
